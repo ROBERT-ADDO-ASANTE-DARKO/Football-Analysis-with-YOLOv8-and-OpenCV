6911988173c01c58569589ce37cb578b4dd39f06
Robert Yaw Agyekum Addo <300621923815@bluecrest.edu.gh>
2024-08-14 16:03:35 +0000

    Added the trained YOLOv5 model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('models/best.pt')
+model = YOLO('/teamspace/studios/this_studio/models/best.pt')
 
 results = model.predict('/teamspace/studios/this_studio/input_video/08fd33_4.mp4', save=True)
 print(results[0])
